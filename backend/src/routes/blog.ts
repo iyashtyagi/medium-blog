aa77a9d6bb65a2cf1363be9c220cc9cd909295ad
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-22 22:42:14 +0530

    added middleware in blog routes

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -8,9 +8,25 @@ export const blogRouter = new Hono<{
     Bindings: BindingTypes
 }>();
 
+blogRouter.use("/*", async (c, next)=>{
+    const token = c.req.header("authorization")?.split(' ')[1] || "";
+    if(!token){
+        return c.json({"message": "please login/signup"}, 401)
+    }
+    
+    try {
+        const result = await verify(token, c.env.JSON_SECRET);
+        // @ts-ignore
+        c.set("userId", result.id);
+        await next();
+    } catch (error) {
+        return c.json({"message": "Invalid token"}, 401)
+    }
+})
 
-blogRouter.post('/', (c) => {
-    return c.text('Hello Hono!')   
+
+blogRouter.post('/', async (c) => {
+
 })
 
 blogRouter.put('/', (c) => {
