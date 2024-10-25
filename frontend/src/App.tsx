bac16ea292c5269a9e3d5852599727838e961fe4
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-26 00:35:24 +0530

    create basic structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
-function App() {
-  return <div>
-    Hello world
-  </div>
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Blog } from "./pages/Blog";
+
+const App = () =>{
+    return <>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/blog" element={<Blog />} />
+        </Routes>
+    </BrowserRouter>
+    </>
 }
 
-export default App
+export default App;
