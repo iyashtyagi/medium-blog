3411ef723b99d0d79a96cf4c135e6d408ad859b2
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-27 23:59:39 +0530

    signup page

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,64 @@
+import { SingupInput } from "@yashtyagi/medium-common"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { InputBox } from "./InputBox"
+import { Button } from "./Button"
+import axios from "axios"
+import { BACKEND_URL } from "../config"
+
+export const Auth = () => {
+
+    const navigate = useNavigate();
+
+    const [postInput, setPostInput ] = useState<SingupInput>({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    async function sendRequest(){ 
+        const singupUrl = `${BACKEND_URL}/user/signup`;
+        try {
+            const response = await axios.post(singupUrl, postInput)
+            const jwt = response.data.token;
+            localStorage.setItem("token", jwt);
+            navigate("/blog");
+        } catch (error) {
+            console.log(error,"from line 27");
+        }
+    }
+
+    return <div className="h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div>
+
+                <div className="px-10 py-6">
+                    <div className="text-3xl font-bold">
+                        Create an account
+                    </div>
+
+                    <div className="text-slate-500 flex justify-center">
+                        Already have an account? <Link to={"/signin"} children="Login" className="underline pl-2"/>
+                    </div>
+                </div>
+
+                <div>
+                    <InputBox label="Name" placeholder="Enter your name" onChange={(e) =>{
+                        setPostInput({...postInput, name:e.target.value})
+                    }} />
+                    <InputBox label="Email" placeholder="me@example.com" onChange={(e) =>{
+                        setPostInput({...postInput, email:e.target.value})
+                    }} />
+                    <InputBox label="Password" type="password" placeholder="12345678" onChange={(e) =>{
+                        setPostInput({...postInput, password:e.target.value})
+                    }} />
+
+                    <Button onClickHandler={sendRequest} label="Sign up"/>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+}
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,5 +1,14 @@
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
+
 export const Signup = () => {
-    return <>
-        Signup
-    </>
+    return <div className="flex justify-between">
+        <div className="w-full md:w-1/2">
+            <Auth />
+        </div>
+
+        <div className="hidden md:block w-1/2">
+            <Quote />
+        </div>
+    </div>
 }
