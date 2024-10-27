dae9694094971938b1fb81cffcd88fc347ca70f5
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-27 23:30:06 +0530

    added Quote component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,18 @@
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="w-3/4 mx-auto"> 
+            <p className="text-2xl font-semibold">
+                "The customer service I recevied was exceptional. The support team went above and beyond to address my concerns."
+            </p>
+
+            <div className="mt-6">
+                <p className="font-semibold">
+                    Jules Winnfield
+                </p>
+                <p className="text-slate-500">
+                    CEO, Acme Inc
+                </p>
+            </div>
+        </div>
+    </div>
+}
