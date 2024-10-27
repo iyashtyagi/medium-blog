836cda241fbbc80ce90ee70216d30b937d134056
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-27 23:15:19 +0530

    added CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
+import { cors } from 'hono/cors';
 
 export interface BindingTypes{
     DATABASE_URL : string;
@@ -13,6 +14,7 @@ export interface VariablesTypes{
 
 const app = new Hono<{Bindings: BindingTypes}>();
 
+app.use("/*", cors());
 app.route('/api/v1/user', userRouter);
 app.route('/api/v1/blog', blogRouter)
 
