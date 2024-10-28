ad9b4124cb9b2cab3bc4d157b926bef809d8c633
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-28 23:42:50 +0530

    added /blogs /blog/:id route in the router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
+import { Blogs } from "./pages/Blogs";
 
 const App = () =>{
     return <>
@@ -9,7 +10,8 @@ const App = () =>{
         <Routes>
             <Route path="/signup" element={<Signup />} />
             <Route path="/signin" element={<Signin />} />
-            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<Blog />} />
+            <Route path="/blogs" element={<Blogs />} />
         </Routes>
     </BrowserRouter>
     </>
