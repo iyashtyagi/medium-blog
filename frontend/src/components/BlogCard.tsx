076892b437eac0cdf36db012dac886f77dede0e2
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-28 23:45:53 +0530

    added BlogCard

---
+++ frontend/src/components/BlogCard.tsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+import { NameAvatar } from "./NameAvatar";
+
+interface CardInputs {
+    id: string;
+    authorName: string;
+    title: string;
+    description: string;
+    publishedDate: string;
+}
+
+export const BlogCard = ({id, authorName, title, description, publishedDate}: CardInputs) => {
+    return <Link to={`/blog/${id}`}>
+        <div className="border-b px-6 lg:px-0 py-5 w-screen max-w-screen-md cursor-pointer">
+            <div className="flex items-center text-sm">
+
+                <div>
+                    <NameAvatar name={authorName} /> 
+                </div>
+
+                <div className="pl-2">
+                    {authorName}
+                </div>
+                
+                <div className="pl-2">
+                    {Circle()}
+                </div>
+
+                <div className="text-slate-400 pl-2">
+                    {publishedDate}
+                </div>
+            </div>
+            
+            <div className="pt-3 text-xl font-semibold ">
+                {title}
+            </div>
+
+            <div className="pt-3 text-md font-light">
+                {description.length>100 ? description.slice(0,300)+"..." : description}
+            </div>
+
+            <div className="text-sm font-light text-slate-400 pt-5">
+                {`${Math.ceil(description.length/100)}`} {description.length>100 ? " minutes read" : "minute"}
+            </div>
+
+        </div>
+    </Link>
+}
+
+function Circle(){
+    return <div className="h-1 w-1 bg-slate-400 rounded-full">
+    </div>
+}
