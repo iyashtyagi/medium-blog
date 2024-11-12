97b84f78bf2c7f7ea2c4823d139c67d891f17238
Yash Tyagi <contactyashtyagi@gmail.com>
2024-11-12 23:24:15 +0530

    updated AppBar to make the Medium logo clickable

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import { AvatarType } from "../types";
 import { NameAvatar } from "./NameAvatar";
 
 export const AppBar = () => {
     return <div className="flex justify-between px-6 pt-3 pb-2 border-b">
         <div className="flex">
-            <div className="text-3xl font-bold">
-                Medium
-            </div>
+            <Link to={"/blogs"}>
+                <div className="text-3xl font-bold cursor-pointer">
+                    Medium
+                </div>
+            </Link>
 
         </div>
         <div>
