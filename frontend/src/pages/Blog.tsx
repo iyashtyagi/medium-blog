95cb929eed0d7fd736dd35eae4d38f51a2f0fcb2
Yash Tyagi <contactyashtyagi@gmail.com>
2024-11-12 23:25:46 +0530

    create the blog page

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,5 +1,22 @@
+import { useParams } from "react-router-dom";
+import { FullBlog } from "../components/FullBlog";
+import { useBlog } from "../hooks"
+import { AppBar } from "../components/AppBar";
+
 export const Blog = () => {
-    return <>
-        Blog
-    </>
+    const { id } = useParams();
+    const { loading, blog } = useBlog({
+        id : id || ""
+    });
+
+    if(loading){
+        return <div>
+            loading...
+        </div>
+    }
+
+    return <div>
+        <AppBar />
+        {blog ? <FullBlog blog={blog} /> : <div>Blog not found</div>}
+    </div>
 }
