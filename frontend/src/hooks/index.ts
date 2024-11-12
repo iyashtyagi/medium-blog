8e73497421b81589568243c20e180e60b51b16af
Yash Tyagi <contactyashtyagi@gmail.com>
2024-11-12 23:23:10 +0530

    added useBlog hook to fetch a particular blog

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -3,6 +3,31 @@ import axios from "axios";
 import { BACKEND_URL } from "../config";
 import { BlogStructure } from "../types";
 
+export const useBlog = ({ id } : { id: string })=>{
+    const [loading, setLoading] = useState<Boolean>(true);
+    const [blog, setBlog] = useState<BlogStructure>();
+
+    useEffect(()=>{
+        axios.get(`${BACKEND_URL}/blog/${id}`,{
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}` 
+            }
+        })
+        .then(res => {
+            setBlog(res.data.blog);
+            setLoading(false);
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    }, [])
+
+    return {
+        loading,
+        blog
+    }
+}
+
 export const useBlogs = () => {
     const [loading, setLoading] = useState<Boolean>(true);
     const [blogs, setBlogs] = useState<BlogStructure[]>([]);
